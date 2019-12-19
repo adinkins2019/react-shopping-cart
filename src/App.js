181386f38d800e7925206e4f13eab4f43ab2c4c3
Antonio Dinkins <50257736+adinkins2019@users.noreply.github.com>
2019-12-18 21:42:33 -0500

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-import {ProductContext} from './contexts/ProductContext'
-import {CartContext} from './contexts/CartContext'
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import {ProductContext} from './contexts/ProductContext'
+import {CartContext} from './contexts/CartContext'
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
-		//console.log(item)
-		setCart([...cart, item])
+		setCart([...cart, item]);
 		console.log(cart)
 	};
-
+	const removeItem = items => {
+		//Remove item from the cart using the product ID
+		
+	}
 	return (
-		<ProductContext.Provider value={{products, addItem}}>
-			<CartContext.Provider value={{cart}}>
-				<div className="App">
-			
-				<Navigation />
+		<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value =  {cart}>
+		<div className="App">
+			<Navigation cart={cart} />
 
-				{/* Routes */}
-					<Route
-						exact
-						path="/"
-						component={Products}
-							/>
-						)}
-					/>
+			{/* Routes */}
+			<Route
+				exact
+				path="/"
+				component={Products}
+				
+			/>
 
-					<Route
-						path="/cart"
-						component={ShoppingCart} />}
-					/>
-			
-				</div>
-			</CartContext.Provider>
+			<Route
+				path="/cart"
+				render={() => <ShoppingCart cart={cart} />}
+			/>
+		</div></CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
